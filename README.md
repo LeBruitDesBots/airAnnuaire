c51169cf6b92ec53b9672eea1824f2bbcb76d1f3
LeBruitDesBots <lebruitdesbots@protonmail.com>
2020-05-28 22:15:31 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# airAnnuaire
+
+Un bot pour aider à indexer, classer et partager les subreddits francophones.
---
+++ Subreddit.py
@@ -0,0 +1,35 @@
+from enum import Enum
+from datetime import datetime
+
+class SubredditStatus(Enum):
+    UNKNOWN = 1
+    ACTIVE = 2
+    EMPTY = 3
+    PRIVATE = 4
+
+class Language(Enum):
+    UNKNOWN = 1
+    FRENCH = 2
+    MULTILINGUAL = 3
+    ENGLISH = 4
+    OTHER = 5
+
+
+class Subreddit:
+    """A class to retrieve and store information about subreddits"""
+
+    def __init__(self, name):
+        self.name = name
+        # Auto-retrieved properties
+        self.status = SubredditStatus.UNKNOWN
+        self.is_nsfw = False
+        self.subscriber_count = 0
+        self.created_utc = 0
+        self.description = ''
+        self.top_mod = ''
+        # Manual properties
+        self.language = Language.UNKNOWN
+        self.tags = list()
+        
+        self.auto_updated = None
+        self.manu_updated = None
